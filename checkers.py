57759dbe2be52b63763403d90f48f50e96b64d46
RepedeMonica <126000692+RepedeMonica@users.noreply.github.com>
2024-12-07 16:27:01 +0200

    add option to choose to play with computer

--- checkers.py
+++ checkers.py
@@ -1,17 +1,23 @@
+"""This module connects the start menu with the window for the game"""
+
+import tkinter as tk
+import sys
 from startMenu import choose_board_size
 from game import GameOfGo
-import tkinter as tk
 
-def main(selected_size=None):
+def main(computer=False, selected_size=None):
     """The entry point of the game"""
     if selected_size is None:
-        choose_board_size()
+        choose_board_size(computer)
     else:
         root = tk.Tk()
-        GameOfGo(root, selected_size)
+        GameOfGo(root, computer, selected_size)
         root.mainloop()
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print(f"Usage: checkers.py <computer/human>")
+        sys.exit(1)
+    main(computer= ("computer"== sys.argv[1]))
 
 
