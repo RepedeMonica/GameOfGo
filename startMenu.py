e76c7de49b3fe8036f89d6141893b0b38488999a
RepedeMonica <126000692+RepedeMonica@users.noreply.github.com>
2024-12-07 16:29:22 +0200

    add pep description for this module

--- startMenu.py
+++ startMenu.py
@@ -1,6 +1,8 @@
+"""This module implements the start menu"""
+
 import tkinter as tk
 
-def choose_board_size():
+def choose_board_size(computer=False):
     """Display the start menu in which the user chooses the board size"""
     def submitted():
         """Handle the chosen table dimension by the player"""
@@ -8,34 +10,35 @@ def choose_board_size():
         print(f"Selected size is {selected_size}")
         root.destroy()
         from checkers import main
-        main(selected_size)
+        main(computer, selected_size)
 
+    COLOR = "tan"
     root = tk.Tk()
-    root.geometry("600x600")
+    root.geometry("1000x700")
     root.title("Welcome to Game of Go!")
 
-    canvas = tk.Canvas(root, width=600, height=600, bg="tan")
+    canvas = tk.Canvas(root, width=600, height=600, bg=COLOR)
     canvas.pack(fill="both", expand=True)
 
-    frame = tk.Frame(canvas, bg="tan")
+    frame = tk.Frame(canvas, bg=COLOR)
     frame.pack(expand=True)
 
     indications = tk.Label(
         frame, text="Please choose the size of the board:",
-        font=("Chiller",28), bg="tan")
+        font=("Chiller",48), bg=COLOR)
     indications.pack()
 
-    second_frame = tk.Frame(frame, bg="tan")
+    second_frame = tk.Frame(frame, bg=COLOR)
     second_frame.pack()
 
     possible_dimensions = tk.Spinbox(
         second_frame, values=["9", "13", "17", "19"],
-        font=("Chiller",28), width=3, bg="tan")
+        font=("Chiller",38), width=3, bg=COLOR)
     possible_dimensions.pack(side=tk.LEFT, padx=5)
 
     button = tk.Button(
-        second_frame, text="Submit", font=("Chiller",20),
-        command=submitted, bg="tan")
+        second_frame, text="Submit", font=("Chiller",30),
+        command=submitted, bg=COLOR)
     button.pack(side=tk.RIGHT)
 
     root.mainloop()
